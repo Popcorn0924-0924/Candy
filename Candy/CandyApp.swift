c037e003f838c99ee8f055d06742d20bc7869e97
jackson <jiechun.lai@gmail.com>
2020-11-20 12:12:08 +0800

    Initial Commit

---
+++ Candy/CandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandyApp.swift
+//  Candy
+//
+//  Created by User02 on 2020/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
